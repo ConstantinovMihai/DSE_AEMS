2a5aa6c1224c2be267e8a14afc673ba4c4d00ae3
ConstantinovMihai <97915789+ConstantinovMihai@users.noreply.github.com>
2022-05-11 10:36:19 +0200

    Implement the Monte Carlo sensitivity analysis, including visualisation. Some more work needs to be done regarding efficiency and beautifying the visualisations.

--- sensitivity_analysis.py
+++ sensitivity_analysis.py
@@ -9,7 +9,8 @@ import scipy.stats as ss
 import matplotlib.pyplot as plt
 from copy import deepcopy
 import os
-
+import pandas as pd
+import seaborn as sns
 
 class Weights:
     def __init__(self) -> None:
@@ -32,14 +33,13 @@ class Weights:
                 self.overall_weights = np.array([float(s) for s in re.findall(r'-?\d+\.?\d*', line)])
             else:
                 line = line.split(":")[1].split("],")
-                #self.weights_crit = [float(s) for s in re.findall(r'-?\d+\.?\d*', line[0])]
                 crits = []       
                 for el in line:
                     crit = [float(s) for s in re.findall(r'-?\d+\.?\d*', el)]
                     crits.append(np.array(crit))
                 self.scores.append(crits)
         # convert the self.scores from a list to an array
-        self.scores = self.scores
+        # self.scores = np.array(self.scores)
            
 
     def partialScores(self):
@@ -50,8 +50,6 @@ class Weights:
         # perform the weighted average sum
         self.partial_scores = []
         for crit in self.scores:
-            print(f"crit is {crit} ")
-            print(f"np.average {np.average(crit[1:], axis=1, weights=crit[0])}")
             self.partial_scores.append(np.average(crit[1:], axis=1, weights=crit[0]))
 
 
@@ -62,6 +60,7 @@ class Weights:
         self.partialScores()
         self.winner = np.argmax(np.average(self.partial_scores, axis=0, weights=self.overall_weights))
         print(self.winner)
+        return self.winner
 
 
 
@@ -71,6 +70,9 @@ class OneAtATime(Weights):
     and scores to one element at a time in order to ensure the trade off is robust
     """
     def __init__(self):
+        """
+        Constructor for the MonteCarlo Class
+        """
         Weights.__init__(self)
 
 
@@ -109,6 +111,7 @@ class OneAtATime(Weights):
         create the increments array
         """
         increments = np.arange(-2, 2, 0.5)
+        # remove the increment = 0 entry in the array
         increments  = np.delete(increments, np.where(increments == 0))
         return increments
 
@@ -131,66 +134,95 @@ class MonteCarlo(Weights):
     It adds some noise in the matrix of weights and checks which design is predicted as optimum
     """
     def __init__(self):
+        """
+        Constructor for the MonteCarlo Class
+        """
         Weights.__init__(self)
-    
 
-    def generateRandomNumbers(self, nb_instances):
+    def generateRandomNumbers(self, nb_instances, deviation=4):
         """ 
         Generate random numbers by drawing numbers from multinomial distributions
         whose probabilities are drawned from normal distribution
-        Returns nb_instances numbers drawn from this distribution
+        :param: deviation - sets the standard deviation - the lower the deviation value, the higher the variance
+        :param: nb_instances - the number of random numbers to be generated
+        Returns a numpy array containg nb_instances numbers drawn from this distribution
         """          
         x = np.arange(-10, 11)
         xU, xL = x + 0.5, x - 0.5 
         prob = ss.norm.cdf(xU, scale = 3) - ss.norm.cdf(xL, scale = 3)
         prob = prob / prob.sum() # normalize the probabilities so their sum is 1
-        nums = np.random.choice(x, size = nb_instances, p = prob) / 4
+        nums = np.random.choice(x, size = nb_instances, p = prob) / deviation
         return nums
     
 
-    def generateNoisyWeights(self):
+    def generateNoisyWeights(self, deviation=4):
         """
         Generates a matrix of weights containg noisy data, 
         used to verify the robustness of the trade-off
+        :param: deviation - sets the standard deviation - the lower the deviation value, the higher the variance
+        Returns a matrix containing noises
         """
         # deep copy the initial matrix, then add noise
         d = deepcopy(self)
 
-        noise = self.generateRandomNumbers(len(d.overall_weights))
+        noise = self.generateRandomNumbers(len(d.overall_weights), deviation)
         d.overall_weights = d.overall_weights + noise
-       #  d.overall_weights[d.overall_weights < 0] = 0
+        d.overall_weights[d.overall_weights < 0] = 0
 
-       
-        noise = self.generateRandomNumbers(len(d.scores[0]) * len(d.scores))
-        # print(f" (len(d.scores), len(d.scores[0])), { len(d.scores)}   {len(d.scores[0])}")
-        # print(f"d.scores is \n{d.scores} ")
-        # d.scores = d.scores + np.reshape(noise, (len(d.scores), len(d.scores[0])))
-        print(f"modified d.scores are \n{d.scores}")
-        #d.scores[d.scores < 0] = 0.01
+        # unfortunately we could not find a way to reshape 
+        # either reshape the list into np.array without errors/deprecation warnings
+        # list comprehension for noise would be possible though, it is something 
+        # TODO: get rid of the matrix element by element iteration
+        for score in d.scores:
+            for row in score:
+                noise = self.generateRandomNumbers(len(row), deviation)
+                row += noise
+            
         return d
 
+    def visualiseResults(self, options, nb_experiments, plot=True):
+        """
+        Functionality to be able to visualise the results of the sensitivity analysis 
+        :param: nb_experiments - the number of Monte Carlo simulations to be performed
+        :param: options - a vector with the values of various options as simulated by the Monte Carlo method
+        :param: plot - if True, it will generate the histogram
+        Generates a histogram with the options values
+        """
+        options = options / nb_experiments * 100
+        print(f"Options1 - {round(options[0], 3)}")
+        print(f"Options2 - {round(options[1], 3)}")
+        print(f"Options3 - {round(options[2], 3)}") 
+        print(f"Total iterations - {nb_experiments}")
+        # plot the histogram
+        print(f"len options {len(options)}")
+        if plot:
+            x = [ "Balloon","VTOL", "N-copter"]
+            freq = options
+            df = pd.DataFrame({"Options":x,'Freq. (%)':freq})
+            df.set_index("Options")['Freq. (%)'].plot.bar(rot=0)
+            plt.show()
+      
 
-    def iterateWeights(self, nb_experiments):
+    def iterateWeights(self, nb_experiments=1000, deviation=4):
         """
         Main method of the class, generates nb_experiments noisy matrices and checks 
         whether or not they predict the same final design option as the "clean" matrix
+        :param: nb_experiments - the number of Monte Carlo simulations to be performed
+        :param: deviation - sets the standard deviation - the lower the deviation value, the higher the variance
         """
+        options = np.zeros(3)
         for i in range(nb_experiments):
             # generate noisy matrix check the final score it predicts
-            d = self.generateNoisyWeights()
-            d.finalScore()
+            d = self.generateNoisyWeights(deviation)
+            option = d.finalScore()
+            options[option] += 1
 
-p = MonteCarlo()
-currentDirectory = str(os.getcwd()) + "\\trade_off.txt"
-#p.readData(r"C:\Users\mihai\OneDrive\Desktop\DSE\trade_off.txt")
-p.readData(currentDirectory)
-p.iterateWeights(100)
+        self.visualiseResults(options, nb_experiments)
 
+        
 
-# p = OneAtATime()
-# p.readData(r"C:\Users\mihai\OneDrive\Desktop\DSE\trade_off.txt")
-# p.oneAtATime()
-
-
-# TODO: implement Monte Carlo Technique
-# TODO: implement Visualisation for the Monte Carlo Techniques
+if __name__ == "__main__":
+    p = MonteCarlo()
+    currentDirectory = str(os.getcwd()) + "\\trade_off.txt"
+    p.readData(currentDirectory)
+    p.iterateWeights()
--- tests.py
+++ tests.py
@@ -2,4 +2,6 @@ import numpy as np
 
 crit = [np.array([5.1, 0, 4.3, 2.4]), np.array([2.4, 4, 4, 2]), np.array([3, 2.2, 2, 2]), np.array([3, 2.2, 2, 4])]
 
+crit = np.array(crit)
+
 print(f"np.average {np.average(crit[1:], axis=1, weights=crit[0])}")
