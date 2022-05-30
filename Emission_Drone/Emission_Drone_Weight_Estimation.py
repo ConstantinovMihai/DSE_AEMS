21a483b7f89a2ffead13e496202c639b5664fe3b
Stein Köbben <s.kobben@student.tudelft.nl>
2022-05-30 10:27:32 +0200

    put sensitivy analysis files in a folder

--- Emission_Drone/Emission_Drone_Weight_Estimation.py
+++ Emission_Drone/Emission_Drone_Weight_Estimation.py
@@ -3,7 +3,11 @@ Initial Weight Estimation for the emission and air pollution weight estimation
 """
 
 def get_payload_mass():
-    sensors_dict = {"CO-CX":8, "NO-B4":13, "SO2-A4":6, "IRC-AT":15, "OPC-R2":30, "NO2-AE":6, "O3-AH":6}
+    sensors_dict = {"CO-CX":8, "NO-B4":13, "SO2-A4":6, "IRC-AT":15, "OPC-R2":30, "NO2-AE":6, "O3-AH":6} # masses in grams
+    support_dict = {"fan":80, "airpump":10, "sensor_adapters":40} # masses in grams
+    mass_support = sum(support_dict.values())
     mass_all_sensors = sum(sensors_dict.values())
-    return mass_all_sensors
+    return mass_all_sensors + mass_support
+
+
 
