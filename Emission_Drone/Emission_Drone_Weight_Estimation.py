156e25f047e41b35ed551fa2ac0120b8af95ea98
akubba <a.kubba@student.tudelft.nl>
2022-05-30 10:31:26 +0200

    Mass Iteration class

--- Emission_Drone/Emission_Drone_Weight_Estimation.py
+++ Emission_Drone/Emission_Drone_Weight_Estimation.py
@@ -2,12 +2,17 @@
 Initial Weight Estimation for the emission and air pollution weight estimation
 """
 
+
 def get_payload_mass():
-    sensors_dict = {"CO-CX":8, "NO-B4":13, "SO2-A4":6, "IRC-AT":15, "OPC-R2":30, "NO2-AE":6, "O3-AH":6} # masses in grams
-    support_dict = {"fan":80, "airpump":10, "sensor_adapters":40} # masses in grams
-    mass_support = sum(support_dict.values())
+    sensors_dict = {"CO-CX": 8, "NO-B4": 13, "SO2-A4": 6, "IRC-AT": 15, "OPC-R2": 30, "NO2-AE": 6, "O3-AH": 6}
     mass_all_sensors = sum(sensors_dict.values())
-    return mass_all_sensors + mass_support
+    return mass_all_sensors
 
 
+class Drone:
+    def __init__(self, total_mass, payload):
+        self.total_mass = total_mass
+        self.payload = payload
 
+    def MassIteration(self):
+        pass
