a4ee7e6bc74934c4f36e6d645c07899e4643ad7e
akubba <a.kubba@student.tudelft.nl>
2022-05-31 11:27:23 +0200

    Flight time function

--- Emission_Drone/Propulsion_Sizing.py
+++ Emission_Drone/Propulsion_Sizing.py
@@ -23,11 +23,11 @@ alpha0 = 0 #Zero-lift angle of attack
 K0 = 6.11
 
 
-
 def thrustPropeller(labda,dzeta,Bp,K0,eta,Hp,alpha0,height,Temp,N,Dp, Cp,**kwargs):
     A = Dp / Cp
     thrust = eq.thrustCoefficient(labda,dzeta,Bp,K0,eta,Hp,Dp,alpha0,A) * eq.rho(height, Temp) * (N/60)**2 * Dp**4
     return thrust
+
 
 def momentPropeller(height, Temp, Cfd,N, Cp, K0, e,eta,Hp,Dp,alpha0, labda, dzeta, Bp):
     A = Dp / Cp
@@ -42,6 +42,7 @@ def motor_U_I(M, N, KV0, Um0, Im0, Rm):
     print(U, I)
     return U, I
 
+
 def check_propellers(propeller_matrix, total_mass, labda,dzeta,K0,eta,alpha0, Cp,**kwargs):
     """
     Calculates whether propeller options can provide enough thrust for T/W = 2
@@ -51,13 +52,17 @@ def check_propellers(propeller_matrix, total_mass, labda,dzeta,K0,eta,alpha0, Cp
     result_mat = []
     for row in propeller_matrix:
         name = row[0]
-        T = thrustPropeller(labda,dzeta,2,K0,eta,row[2],alpha0,height,Temp,row[3],row[1],Cp)
+        T = thrustPropeller(labda, dzeta,2,K0,eta,row[2],alpha0, height, Temp,row[3],row[1],Cp)
         if T > required_thrust:
             result_mat.append([name, "yes"])
         else:
             result_mat.append([name, "no"])
 
     return result_mat # return matrix with [[prop name, yes/no],[prop name, yes/no],[prop name, yes/no]]
+
+
+def flight_time(battery_w,  battery_cap, frame_w, no_propellers, prop_eff):
+    return ((battery_cap*battery_w)/(frame_w+battery_w))*prop_eff*((frame_w+battery_w)/no_propellers)
 
 test_mat = [["APC 6×4.1SF", 0.1524, 0.10414, 20000],["T-Motor SW 13x5", 0.3302, 0.127, 9600],\
             ["APC 11x12E", 0.2794, 0.3048, 13636.36364]]
