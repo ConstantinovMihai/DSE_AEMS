b1fcca26da0db6395aa66453a4f5280576181ab4
Bahier <74194445+Bahier@users.noreply.github.com>
2022-06-02 11:35:16 +0200

    Propeller Efficiency done

--- Emission_Drone/Propulsion_Sizing.py
+++ Emission_Drone/Propulsion_Sizing.py
@@ -17,8 +17,14 @@ def matrix_gen(SheetNumber):
     df = pd.read_excel(r'.\Propellers.xlsx', sheet_name = SheetNumber)
     a = df.to_numpy()
     return a
-propellerMatrix=matrix_gen(0)
 
+propellerMatrix = matrix_gen(0)
+sixinchprops = matrix_gen(2)
+seveninchprops = matrix_gen(3)
+eightinchprops = matrix_gen(4)
+nineinchprops = matrix_gen(5)
+teninchprops = matrix_gen(6)
+eleveninchprops = matrix_gen(7)
 
 "C O N S T A N T S"
 eta = 0.85 #Correction factor for downwash
@@ -32,7 +38,7 @@ K0 = 6.11 #Cl-alpha [rad^-1]
 totalweight=3.028*9.81
 
 
-def propellerThrust(propellerMatrix, labda, dzeta, K0, eta, alpha0, total_weight, **kwargs):
+def propellerThrust(labda, dzeta, K0, eta, alpha0,Dp,Hp,N,**kwargs):
     """
     :param: labda - [-]
     :param: dzeta - [-]
@@ -42,37 +48,16 @@ def propellerThrust(propellerMatrix, labda, dzeta, K0, eta, alpha0, total_weight
     :param: Hp - [m]
     :param: Dp - [m]
     :param: alpha0 - [rad]
-    :param: total_weight - [kg]
     """
-
     A = 5  # set the aspect ratio
-    for row in propellerMatrix:  # compute the performance of each prop
-        names = row[0]
-        Dp = row[1]
-        Hp = row[2]
-        maxrpm = row[3]
-        N = np.arange(10, maxrpm, 10)
-        thrust = eq.thrustCoefficient(labda, dzeta, 2, K0, eta, Hp, Dp, alpha0, A) * 1.225 * (N/60)**2 * Dp**4
-
-        # prune the thrust values based on maximum (climb) and then plot
-        if thrust[-1] > (total_weight*9.81) / 2:
-            plt.plot(N, thrust, label=names)
-
-    plt.xlabel("RPM")
-    plt.ylabel("Thrust in N")
-    plt.ylim(0, 17)
-    plt.axhline((total_weight*9.81)/4, linestyle="dotted", label="Thover")
-    plt.axhline((total_weight*9.81)/2, linestyle="dashed", label="Tmax")
-    plt.legend()
-    plt.show()
-
-    return
+    thrust = eq.thrustCoefficient(labda, dzeta, 2, K0, eta, Hp, Dp, alpha0, A) * 1.225 * (N/60)**2 * Dp**4
+    return thrust
 
 
-propellerThrust(propellerMatrix[1:28, 1:5], labda, dzeta, K0, eta, alpha0, 3.028)
 
 
-def propellerTorque(propellerMatrix,Cfd, K0, e,eta,Hp,Dp,alpha0, labda, dzeta, N):
+
+def propellerTorque(Cfd, K0, e, eta, alpha0, labda, dzeta, Hp, Dp, N):
     """
     :param: labda - [-]
     :param: dzeta - [-]
@@ -85,20 +70,89 @@ def propellerTorque(propellerMatrix,Cfd, K0, e,eta,Hp,Dp,alpha0, labda, dzeta, N
     :param: Cfd - [-]
     """
     A = 5
+    C_d = eq.dragCoefficient(Cfd, A, K0, e, eta, Hp, Dp, alpha0)
+    torque = eq.momentCoefficient(C_d, A, labda, dzeta, 2) * 1.225 * (N/60)**2 * Dp**5
+    return torque
+
+
+
+
+def propellerComparison(propellerMatrix, labda, dzeta, K0, eta, alpha0,total_weight):
+
+    viableoptions= {}
+    result1mat=[]
+    result2mat = [ ]
+    torquevalues = []
+    fig, axs = plt.subplots(2)
     for row in propellerMatrix:
-        names=row[0]
-        Dp=row[1]
-        Hp=row[2]
-        maxrpm=row[3]
-        N=np.arange(10,maxrpm,10)
-        C_d = eq.dragCoefficient(Cfd, A, K0, e, eta, Hp, Dp, alpha0)
-        moment = eq.momentCoefficient(C_d, A, labda, dzeta, 2) * 1.225 * (N/60)**2 * Dp**5
-        plt.plot(N, moment, label=names)
-    plt.xlabel("RPM")
-    plt.ylabel("Moment in Nm")
-    plt.legend()
+        names = row[ 0 ]
+        Dp = row[ 1 ]
+        Hp = row[ 2 ]
+        maxrpm = row[ 3 ]
+
+        N = np.arange(100, maxrpm, 100)
+
+        thrust = propellerThrust(labda, dzeta, K0, eta, alpha0,Dp,Hp,N)
+        if thrust[ -1 ] > (total_weight * 9.81) / 2:
+            axs[0].plot(N, thrust, label=names)
+
+            found = [False, False]
+            for idx, val in enumerate(thrust):
+                if val >=  (total_weight * 9.81) / 4 and val <= 1.05 * (total_weight * 9.81) / 4 and not found[0]:
+                    found[0] = True
+                    rpmNominalThrust = N[idx]
+                    result1mat.append([ names, Dp, Hp, maxrpm, rpmNominalThrust ])
+                    axs[ 0 ].axvline(rpmNominalThrust, linestyle="dotted", c=np.random.rand(3, ),
+                                     label=names + "Nom Thrust RPM")
+                if val >=  (total_weight * 9.81) / 2 and val <= 1.05 * (total_weight * 9.81) / 2 and not found[1]:
+                    found[1] = True
+                    rpmMaxThrust = N[ idx ]
+                    result2mat.append([ names, Dp, Hp, maxrpm, rpmMaxThrust ])
+                    axs[ 0 ].axvline(rpmMaxThrust, linestyle="dotted",c=np.random.rand(3,),  label=names+"Max Thrust RPM")
+        axs[ 0 ].set_xlabel("RPM")
+        axs[ 0 ].set_ylabel("Thrust in N")
+        axs[ 0 ].set_ylim(0, 17)
+    for i in range(len(result1mat)):
+        result1mat[i]=result1mat[i]+[result2mat[i][4]]
+
+    print(result1mat)
+    for entry in result1mat:
+        names1 = entry[ 0 ]
+        Dp1 = entry[ 1 ]
+        Hp1 = entry[ 2 ]
+        maxRpm = entry[ 3 ]
+        nomRpm = entry[4]
+        maxThrustRpm = entry[5]
+        N1 = np.arange(100, maxRpm, 100)
+        torque = propellerTorque(Cfd, K0, e, eta, alpha0, labda, dzeta, Hp1, Dp1, N1)
+        axs[ 1 ].plot(N1, torque, label=names1)
+
+        axs[ 1 ].set_xlabel("RPM")
+        axs[ 1 ].set_ylabel("Torque in Nm")
+        axs[ 1 ].axvline(entry[4], linestyle="dotted",c=np.random.rand(3,),  label=names1+"Nominal Rpm")
+        axs[ 1 ].legend(fontsize=7, loc=2)
+
+        nomtorque = propellerTorque(Cfd, K0, e, eta, alpha0, labda, dzeta, Hp1, Dp1, nomRpm)
+        maxtorque = propellerTorque(Cfd, K0, e, eta, alpha0, labda, dzeta, Hp1, Dp1, maxThrustRpm)
+        torquevalues.append([names1, Dp1, Hp1, maxRpm, nomRpm,maxThrustRpm, nomtorque,maxtorque ])
+
+    axs[ 0 ].axhline((total_weight * 9.81) / 4, linestyle="dotted", label="Thover")
+    axs[ 0 ].axhline((total_weight * 9.81) / 2, linestyle="dashed", label="Tmax")
+    axs[ 0 ].legend(fontsize=7, loc=2)
     plt.show()
-    return
+
+
+
+    return print(torquevalues)
+
+propellerComparison(propellerMatrix[1:28, 1:5], labda, dzeta, K0, eta, alpha0,3.028)
+#propellerComparison(eleveninchprops, labda, dzeta, K0, eta, alpha0,3.028)
+
+
+
+
+
+
 
 def motor_U_I(M, N, KV0, Um0, Im0, Rm):
     U = eq.motorVoltage(M, KV0, Um0, Im0, N, Rm)
