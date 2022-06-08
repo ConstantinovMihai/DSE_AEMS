2fbc6e8356a1940d284c861733ec29d9b62c04f2
Stein Köbben <s.kobben@student.tudelft.nl>
2022-06-08 13:27:49 +0200

    weight

--- Emission_Drone/Emission_Drone_Weight_Estimation.py
+++ Emission_Drone/Emission_Drone_Weight_Estimation.py
@@ -13,9 +13,9 @@ class Drone:
     @staticmethod
     def get_initial_mass_est():
         payload_mass = Drone.get_payload_mass()
-        weights_dict = {"structure": 800, "motors": 352, "propellers": 60, "flight_controllers": 49,\
-                        "ESC's": 88, "Arduino": 25, "battery": 792, "wiring": 100, "Obstacle avoidance": 90,\
-                       "payload": payload_mass}
+        weights_dict = {"structure": 800, "motors": 352, "propellers": 180.3029472, "flight_controllers": 49,\
+                        "ESC's": 88, "Arduino": 25, "battery": 768, "wiring": 200, "Obstacle avoidance": 90,\
+                       "payload": payload_mass, "raspberri Pi": 50, "GPS module": 68, "Jorts transmitter": 20}
         return weights_dict
 
     def __init__(self):
